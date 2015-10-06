e257c1eb5e5d4858d34faa9ff1d2c0fcf20f8e7f
Kareobana <kareobana@atarabi.com>
2015-10-06 18:53:39 +0900

    Added removeFolder

--- src/utils/filesystem.ts
+++ src/utils/filesystem.ts
@@ -1,13 +1,14 @@
 /// <reference path="../../typings/aftereffects/ae.d.ts" />
+/// <reference path="utility.ts" />
 
 namespace KIKAKU.Utils {
-	
+
 	export function getProjectFile() {
 		return app.project.file;
 	}
-	
-	export function createFolder(path: string) {
-		let folder = new Folder(path);
+
+	export function createFolder(path: string | Folder) {
+		let folder = isString(path) ? new Folder(<string>path) : <Folder>path;
 		let folders: Folder[] = [];
 
 		while (!folder.exists) {
@@ -21,5 +22,24 @@ namespace KIKAKU.Utils {
 			}
 		}
 	}
-	
+
+	export function removeFolder(path: string | Folder) {
+		function _removeFolder(folder: Folder) {
+			let files = folder.getFiles() || [];
+			forEach(files, (file: File | Folder) => {
+				if (file instanceof Folder) {
+					_removeFolder(file);
+				} else {
+					file.remove();
+				}
+			});
+			folder.remove();
+		}
+
+		let folder = isString(path) ? new Folder(<string>path) : <Folder>path;
+		if (folder.exists) {
+			_removeFolder(folder);
+		}
+	}
+
 }
